c30acf7ce1c3bb60683721fb820410b475a4202d
Ned Twigg <ned.twigg@diffplug.com>
2020-06-05 15:57:43 -0700

    Switch license headers to templated years.

--- src/main/resources/spotless/license-confidential.java
+++ src/main/resources/spotless/license-confidential.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 DiffPlug, LLC - All Rights Reserved
+ * Copyright (C) $YEAR DiffPlug, LLC - All Rights Reserved
  * Unauthorized copying of this file via any medium is strictly prohibited.
  * Proprietary and confidential.
  * Please send any inquiries to Ned Twigg <ned.twigg@diffplug.com>
